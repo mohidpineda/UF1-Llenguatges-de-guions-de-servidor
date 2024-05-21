ca3da8b64d9ac59726b8e3e6f0c840adc67934d5
mohidpineda <mohid.ahmad@inslapineda.cat>
2024-05-21 03:26:12 +0200

    comprobacion y creacion de la base de datos

--- conn_db.php
+++ conn_db.php
@@ -4,7 +4,13 @@ $usuario = "mohid";
 $contra = "0000";
 $bbdd = "gestor_tareas";
 
-$conexion_db = new mysqli($ipservidor, $usuario, $contra, $bbdd);
+$conexion_db = new mysqli($ipservidor, $usuario, $contra);
+
+$crear_db = "CREATE DATABASE IF NOT EXISTS gestor_tareas";
+
+mysqli_query($conexion_db, $crear_db);
+
+mysqli_select_db($conexion_db, $bbdd);
 
 return $conexion_db;
 ?>
